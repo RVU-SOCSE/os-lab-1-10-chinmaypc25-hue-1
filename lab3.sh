38093edb8132888c7752e3932be129287d09733d
chinmaypc25-hue <chinmaypcbca25@rvu.edu.in>
2026-04-08 18:17:21 +0530

    Add lab3.sh script for displaying system information

---
+++ lab3.sh
@@ -0,0 +1,18 @@
+echo "job 1 : Display Date"
+date
+sleep 3
+echo "job 2: Display current user"
+whoami
+sleep 3
+echo "job 3 :Display calendar"
+cal
+sleep 2
+echo "job 4: list all the files"
+ls
+sleep 3
+echo "job 5: Display my system date"
+date "%T"
+sleep 1
+echo "job 6"
+pwd
+echo "all jobs are finished"
